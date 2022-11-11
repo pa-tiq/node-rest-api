9a5c26a12f3ac93920481b1e9fe126b2332aaffb
Patrick Dantas <patrickdsilva99@gmail.com>
2022-11-10 20:50:31 -0400

    communicating

--- controllers/feed_controller.js
+++ controllers/feed_controller.js
@@ -1,17 +1,28 @@
 exports.getPosts = (req, res, next) => {
   // 200 = success
   res.status(200).json({
-    posts: [{ title: "oieeeee", content: "diaaaaaaaaaaaaaaaaaaaaaaaaaaa" }],
+    posts: [
+      {
+        _id:'1',
+        title: "oieeeee",
+        content: "diaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        imageUrl: "images/pepe.jpg",
+        creator: {
+          name: "Zé Buceta"
+        },
+        createdAt: new Date()
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title,content);
+  console.log(title, content);
   // 201 = success, a resource was created
   res.status(201).json({
-    message: 'Post created successfully',
-    post: {id: new Date().toISOString(), title: title, content:content }
+    message: "Post created successfully",
+    post: { id: new Date().toISOString(), title: title, content: content },
   });
-}
+};
