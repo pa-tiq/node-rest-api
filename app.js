95cdda79e41b243c6b3ce5216dadb361730cf7ab
pa-tiq <patrickdsilva99@gmail.com>
2022-11-11 10:08:37 -0400

    install mongoose

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
 const feedRoutes = require("./routes/feed_router");
 const app = express();
 
