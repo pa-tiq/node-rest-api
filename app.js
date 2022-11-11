ff0c56aea98985204140ef8b9c6a21b8d4c6f79c
Patrick Dantas <patrickdsilva99@gmail.com>
2022-11-10 20:19:06 -0400

    posts

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const feedRoutes = require("./routes/feed");
-
 const app = express();
 
 app.use(bodyParser.json()); // parse incoming JSON data
